11ebddd5165024be6c855dfc92c8630356ec8dad
Graeme Gregory <gg@slimlogic.co.uk>
2012-10-25 11:00:49 +0100

    power: twl6030_bci_battery add new charger driver
    This patch adds support for the battery charging component of the
    Phoenix IC. It enables charging via USB.
    
    It handles the software driven driver of the twl6030 and the hardware
    controlled driver of the twl6032. The modes are automatically detected
    from the OTP data of the chip.
    
    Signed-off-by: Nishant Kamat <nskamat@ti.com>
    Signed-off-by: Balaji T K <balajitk@ti.com>
    Signed-off-by: Oleksandr Kozaruk <oleksandr.kozaruk@ti.com>
    Signed-off-by: Huzefa Kankroliwala <huzefank@ti.com>
    Signed-off-by: Ambresh K <ambresh@ti.com>
    Signed-off-by: Graeme Gregory <gg@slimlogic.co.uk>
    Signed-off-by: Volodymyr Riazantsev <v.riazantsev@ti.com>
    Signed-off-by: Sergii Postulga <x0153364@ti.com>
    Signed-off-by: Eugen Mandrenko <ievgen.mandrenko@ti.com>
    Signed-off-by: Oleksandr Dmytryshyn <oleksandr.dmytryshyn@ti.com>
    Signed-off-by: Mykola Oleksiienko <x0174904@ti.com>
    Signed-off-by: Grygorii Strashko <grygorii.strashko@ti.com>
    Signed-off-by: Taras Kondratiuk <taras@ti.com>
    Signed-off-by: Sivakumar Pothireddy <sivakumar.pothireddy@ti.com>
    Signed-off-by: Anand Gadiyar <gadiyar@ti.com>

--- include/linux/i2c/twl.h
+++ include/linux/i2c/twl.h
@@ -71,6 +71,7 @@
 #define TWL4030_MODULE_PM_RECEIVER	0x15
 #define TWL4030_MODULE_RTC		0x16
 #define TWL4030_MODULE_SECURED_REG	0x17
+#define TWL6032_MODULE_CHARGER		0x18
 
 #define TWL_MODULE_USB		TWL4030_MODULE_USB
 #define TWL_MODULE_AUDIO_VOICE	TWL4030_MODULE_AUDIO_VOICE
@@ -81,7 +82,9 @@
 #define TWL_MODULE_PM_RECEIVER	TWL4030_MODULE_PM_RECEIVER
 #define TWL_MODULE_RTC		TWL4030_MODULE_RTC
 #define TWL_MODULE_PWM		TWL4030_MODULE_PWM0
+#define TWL6030_MODULE_CHARGER TWL4030_MODULE_MAIN_CHARGE
 
+#define TWL6030_MODULE_GASGAUGE 0x0B
 #define TWL6030_MODULE_ID0	0x0D
 #define TWL6030_MODULE_ID1	0x0E
 #define TWL6030_MODULE_ID2	0x0F
@@ -564,6 +567,23 @@ struct twl4030_clock_init_data {
 struct twl4030_bci_platform_data {
 	int *battery_tmp_tbl;
 	unsigned int tblsize;
+
+	unsigned int monitoring_interval;
+
+	unsigned int max_charger_currentmA;
+	unsigned int max_charger_voltagemV;
+	unsigned int termination_currentmA;
+	unsigned int max_battery_capacity;
+
+	unsigned int max_bat_voltagemV;
+	unsigned int low_bat_voltagemV;
+
+	unsigned int sense_resistor_mohm;
+
+	/* twl6032 */
+	unsigned long features;
+
+	unsigned long errata;
 };
 
 /* TWL4030_GPIO_MAX (18) GPIOs, with interrupts */
